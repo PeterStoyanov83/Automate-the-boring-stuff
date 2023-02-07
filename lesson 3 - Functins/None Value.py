901b737a4d108897dfac3bd07be89578a921169a
petar.stoyanov <peterstoyanov83@gmail.com>
2023-02-07 13:34:49 +0100

    learning

---
+++ lesson 3 - Functins/None Value.py
@@ -0,0 +1,11 @@
+
+
+
+'''None is a representation of the lack of information'''
+
+
+spam = print()
+
+spam == None
+
+
---
+++ lesson 3 - Functins/plusOne.py
@@ -0,0 +1,6 @@
+def plusOne(number):
+    return number + 1
+
+
+new_number = plusOne(5)
+print(new_number)
